ecd10d4b31f9740e37a6b9ecb49adc1e540b427f
Lucas Fonseca <lucasfreireri@gmail.com>
2021-08-13 07:30:44 -0300

    Added Settings module to game

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class do store all settings for Alien Invasio."""
+
+    def __init__(self) -> None:
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230 ,230, 230)
